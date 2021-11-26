0ff2726220d20a69d972c30dc623c66646a5446b
Rehan Guha <rehanguha29@gmail.com>
2021-11-26 14:25:24 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/rehanguha/brisque",
-    package_dir={'': 'brisque'},
+    package_dir={'brisque': 'brisque'},
     packages=setuptools.find_packages(),
     keywords = ['quality', 'svm', 'image', 'maths'],
     classifiers=[
