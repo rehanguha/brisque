4b50ff52db76aca264354e23b0c6292cf060c754
Rehan Guha <rehanguha29@gmail.com>
2021-11-26 19:05:34 +0530

    v0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="brisque", # Replace with your own username
-    version="0.0.7",
+    version="0.0.8",
     author="Rehan Guha",
     py_modules=["brisque"],
     license='mit',
