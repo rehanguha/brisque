284ff85849088ee9404b627ac362bb4dc46560b4
Rehan Guha <rehanguha29@gmail.com>
2022-09-15 16:18:18 +0530

    Update test_brisque.py

--- brisque/test_brisque.py
+++ brisque/test_brisque.py
@@ -6,5 +6,5 @@ def test_validate_url_score():
 
     obj = BRISQUE("https://www.mathworks.com/help/examples/images/win64/CalculateBRISQUEScoreUsingCustomFeatureModelExample_01.png", 
             url=True)
-    assert round(obj.score(),3) == round(74.41910327611319, 3)
+    assert round(obj.score(),3) == round(71.73427549219397, 3)
     
